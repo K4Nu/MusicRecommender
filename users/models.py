5ebf7f49632843bf978417c66a138463b1549164
K4 <k4nu420@gmail.com>
2025-10-28 21:38:06 +0100

    feat(user): create user manager and custom user model

---
+++ users/models.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+
+
+class MyUserManager(BaseUserManager):
+    """
+    A custom user manager to deal with emails as unique identifiers for auth
+    instead of usernames. The default that's used is "UserManager"
+    """
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The email must be set')
+        if not password:
+            raise ValueError('The password must be set')
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValueError('Invalid email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_active", True)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    is_staff = models.BooleanField(
+        default=False)
+    is_active = models.BooleanField(
+        default=True)
+
+    USERNAME_FIELD = 'email'
+    objects = MyUserManager()
+
+    def __str__(self):
+        return self.email
