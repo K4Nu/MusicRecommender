707215389fa5798621781de5622ed6468275f9f1
K4 <k4nu420@gmail.com>
2026-02-03 14:36:50 +0100

    feat(custom-model): Artist model constraints

--- users/models.py
+++ users/models.py
@@ -140,6 +140,15 @@ class Artist(models.Model):
     popularity=models.IntegerField(null=True)
     image_url=models.URLField(null=True,blank=True)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["spotify_id"],
+                condition=Q(spotify_id__isnull=False),
+                name="unique_spotify_id_not_null"
+            )
+        ]
+
     def __str__(self):
         return self.name
 
