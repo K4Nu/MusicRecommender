c8a0db6ee8fe968f229c43a5bc7bba01008a7f18
K4 <k4nu420@gmail.com>
2026-02-03 21:10:06 +0100

    feat(recommendations/services): ColdStartTrack Serializer created

--- recomendations/views.py
+++ recomendations/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions, status
-from .services.cold_start import cold_start_fetch_spotify_global, cold_start_fetch_spotify_viral
+from .services.cold_start import cold_start_fetch_spotify_global
 
 class ColdTest(APIView):
     permission_classes = [permissions.AllowAny]
 
     def get(self,request):
-        cold_start_fetch_spotify_viral.delay()
+        cold_start_fetch_spotify_global.delay()
         return Response(
             {"message": "Cold start"},
             status=status.HTTP_200_OK
