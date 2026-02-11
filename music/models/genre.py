3350ae25b8e7ae4d9c5eb26401ee1434a4e80ec6
K4 <k4nu420@gmail.com>
2026-02-11 23:29:26 +0100

    feat(music/models): Indexes for genre and artist models

--- music/models/genre.py
+++ music/models/genre.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 class Genre(models.Model):
-    name=models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True, db_index=True)  # ← Added unique + db_index
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name'], name='genre_name_idx'),
+        ]
 
     def __str__(self):
         return self.name
